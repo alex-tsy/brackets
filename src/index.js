a0882bd67697d1d4e633701ca2fb49cbf2144b6e
alex-tsy <68004960+alex-tsy@users.noreply.github.com>
2020-10-06 17:48:28 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = new Array();
+    let cfg = new Map(bracketsConfig);
+
+    for (let i = 0; i < str.length; i++) {
+        let top = stack[stack.length - 1];
+        let current = str[i];
+        let isPairOfDifferentBrackets = cfg.has(current) && current != cfg.get(current);
+        let isPairOfIdenticalBrackets = cfg.has(current) && current == cfg.get(current);
+
+        if (isPairOfDifferentBrackets || (isPairOfIdenticalBrackets && top != current)) {
+            stack.push(current);
+        } else if (current != cfg.get(top)) {
+            return false;
+        }else{
+            stack.pop();
+        }
+        
+    }
+    return stack.length == 0;
 }
